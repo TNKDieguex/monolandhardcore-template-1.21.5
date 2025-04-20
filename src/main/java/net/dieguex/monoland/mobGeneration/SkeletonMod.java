c4d82402bb9996b5e64b10f4a2e7714991fd5b77
TNKDieguexPC <diegoaguilar20040303@gmail.com>
2025-04-20 16:58:26 -0400

    Add SkeletonMod and SkeletonFactory to enhance skeleton spawning with custom roles and attributes

---
+++ src/main/java/net/dieguex/monoland/mobGeneration/SkeletonMod.java
@@ -0,0 +1,37 @@
+package net.dieguex.monoland.mobGeneration;
+
+import net.dieguex.monoland.timeManager.ModTimeManager;
+import net.dieguex.monoland.util.SkeletonFactory;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents;
+import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.entity.mob.SkeletonEntity;
+import net.minecraft.entity.mob.WitherSkeletonEntity;
+import net.minecraft.server.world.ServerWorld;
+
+public class SkeletonMod {
+
+    public static void register() {
+
+        ServerEntityEvents.ENTITY_LOAD.register((entity, world) -> {
+            if (!(world instanceof ServerWorld serverWorld))
+                return;
+
+            if (!(entity instanceof SkeletonEntity skeleton) || entity instanceof WitherSkeletonEntity)
+                return;
+            if (entity.getCommandTags().contains("custom_skeleton"))
+                return;
+
+            if (ModTimeManager.hasPassedDays(0)) {
+                MobEntity upgraded = SkeletonFactory.spawnCustomSkeleton(serverWorld, skeleton.getBlockPos());
+
+                if (upgraded != null) {
+                    upgraded.setYaw(skeleton.getYaw());
+                    upgraded.setPitch(skeleton.getPitch());
+                    skeleton.discard();
+                }
+
+                return;
+            }
+        });
+    }
+}
