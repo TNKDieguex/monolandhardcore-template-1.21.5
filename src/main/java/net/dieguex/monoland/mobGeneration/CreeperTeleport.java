e519e34865e3ccd75254d46d62e55af984f09377
TNKDieguexPC <diegoaguilar20040303@gmail.com>
2025-04-20 20:20:18 -0400

    Add teleport sound effect for Creeper teleportation to enhance player experience

--- src/main/java/net/dieguex/monoland/mobGeneration/CreeperTeleport.java
+++ src/main/java/net/dieguex/monoland/mobGeneration/CreeperTeleport.java
@@ -2,6 +2,8 @@ package net.dieguex.monoland.mobGeneration;
 
 import net.fabricmc.fabric.api.entity.event.v1.ServerLivingEntityEvents;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.entity.mob.CreeperEntity;
 import net.minecraft.particle.ParticleTypes;
 import net.minecraft.util.math.BlockPos;
@@ -37,6 +39,15 @@ public class CreeperTeleport {
             BlockPos above = pos.up();
             if (!world.getBlockState(pos).isAir() && world.getBlockState(above).isAir()) {
                 creeper.teleport(x, y, z, true);
+                world.playSound(
+                        null,
+                        creeper.getX(),
+                        creeper.getY(),
+                        creeper.getZ(),
+                        SoundEvents.ENTITY_ENDERMAN_TELEPORT,
+                        SoundCategory.HOSTILE,
+                        1.0f,
+                        1.0f);
 
                 world.spawnParticles(ParticleTypes.PORTAL, x, y, z, 30, 0.5, 0.5, 0.5, 0.1);
                 return;
