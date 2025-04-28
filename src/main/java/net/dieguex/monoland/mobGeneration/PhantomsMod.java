03112f959d969d688148b3da2ac270f70e863a22
TNKDieguexPC <diegoaguilar20040303@gmail.com>
2025-04-27 21:28:17 -0400

    Add PhantomsMod class to manage PhantomEntity health based on in-game days

---
+++ src/main/java/net/dieguex/monoland/mobGeneration/PhantomsMod.java
@@ -0,0 +1,25 @@
+package net.dieguex.monoland.mobGeneration;
+
+import net.dieguex.monoland.timeManager.ModTimeManager;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents;
+import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.mob.PhantomEntity;
+import net.minecraft.server.world.ServerWorld;
+
+public class PhantomsMod {
+    public static void register() {
+        ServerEntityEvents.ENTITY_LOAD.register((entity, world) -> {
+            if (!(world instanceof ServerWorld))
+                return;
+
+            if (entity instanceof PhantomEntity phantom) {
+                if (ModTimeManager.hasPassedDays(6)) {
+
+                    phantom.getAttributeInstance(EntityAttributes.MAX_HEALTH).setBaseValue(40.0);
+                    phantom.setHealth(40.0f);
+                }
+            }
+        });
+
+    }
+}
