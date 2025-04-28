99c848166941d9c5d69c9768d019ed5f7210f800
TNKDieguexPC <diegoaguilar20040303@gmail.com>
2025-04-27 21:30:25 -0400

    Add SkeletonFactory and SpiderHelper for custom skeleton spawning and riding mechanics

---
+++ src/main/java/net/dieguex/monoland/mobGeneration/mobsAbilities/SpiderHelper.java
@@ -0,0 +1,25 @@
+package net.dieguex.monoland.mobGeneration.mobsAbilities;
+
+import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.entity.mob.SpiderEntity;
+import net.minecraft.server.world.ServerWorld;
+
+public class SpiderHelper {
+    public static void addSkeletonRider(ServerWorld world, SpiderEntity spider) {
+        if (!spider.getPassengerList().isEmpty()) {
+            return;
+        }
+        MobEntity skeleton = SkeletonFactory.spawnCustomSkeleton(world, spider.getBlockPos());
+        if (skeleton != null) {
+            skeleton.refreshPositionAndAngles(
+                    spider.getX(),
+                    spider.getY() + 1,
+                    spider.getZ(),
+                    spider.getYaw(),
+                    spider.getPitch());
+
+            world.spawnEntity(skeleton);
+            skeleton.startRiding(spider, true);
+        }
+    }
+}
