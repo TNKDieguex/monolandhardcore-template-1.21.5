f71a24881daa854e9f2bdd05538547729959899b
TNKDieguexPC <diegoaguilar20040303@gmail.com>
2025-05-02 23:17:35 -0400

    Add EnchantUtils class for safe enchantment application and utility methods

---
+++ src/main/java/net/dieguex/monoland/util/EnchantUtils.java
@@ -0,0 +1,50 @@
+package net.dieguex.monoland.util;
+
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.Enchantments;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.util.Identifier;
+import net.minecraft.world.World;
+
+import java.util.Optional;
+
+public class EnchantUtils {
+
+    /**
+     * Aplica un encantamiento seguro a un ItemStack.
+     *
+     * @param world El mundo (para obtener el registry manager)
+     * @param stack El objeto que será encantado
+     * @param key   El RegistryKey del encantamiento (ej: Enchantments.PROTECTION)
+     * @param level El nivel del encantamiento
+     */
+    public static void applyEnchantment(World world, ItemStack stack, RegistryKey<Enchantment> key, int level) {
+        RegistryWrapper.WrapperLookup registryLookup = world.getRegistryManager();
+        RegistryWrapper<Enchantment> enchantmentRegistry = registryLookup.getOrThrow(RegistryKeys.ENCHANTMENT);
+
+        Optional<RegistryEntry.Reference<Enchantment>> enchantmentEntryOpt = enchantmentRegistry.getOptional(key);
+        enchantmentEntryOpt.ifPresent(enchantmentEntry -> stack.addEnchantment(enchantmentEntry, level));
+    }
+
+    /**
+     * Crea un RegistryKey<Enchantment> a partir de un nombre como "sharpness"
+     */
+    public static RegistryKey<Enchantment> key(String name) {
+        return RegistryKey.of(RegistryKeys.ENCHANTMENT, Identifier.of("minecraft", name));
+    }
+
+    /**
+     * Aplica múltiples encantamientos rápidamente
+     */
+    public static void applyMultiple(World world, ItemStack stack, Object[][] enchants) {
+        for (Object[] entry : enchants) {
+            String enchantId = (String) entry[0];
+            int level = (int) entry[1];
+            applyEnchantment(world, stack, key(enchantId), level);
+        }
+    }
+}
