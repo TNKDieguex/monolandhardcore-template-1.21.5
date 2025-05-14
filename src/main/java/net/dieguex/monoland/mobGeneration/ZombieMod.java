46533c30bba2845a55169e11c71fb14ed1782781
TNKDieguexPC <diegoaguilar20040303@gmail.com>
2025-05-13 20:02:52 -0400

    Enhance ZombieMod to equip custom armor and increase damage for Drowned entities based on game days

--- src/main/java/net/dieguex/monoland/mobGeneration/ZombieMod.java
+++ src/main/java/net/dieguex/monoland/mobGeneration/ZombieMod.java
@@ -1,15 +1,29 @@
 package net.dieguex.monoland.mobGeneration;
 
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.attribute.EntityAttributeInstance;
+import net.minecraft.entity.attribute.EntityAttributes;
+
+import java.util.List;
+
 import net.dieguex.monoland.timeManager.ModTimeManager;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents;
 import net.minecraft.entity.mob.GiantEntity;
 import net.minecraft.entity.mob.ZombieEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.random.Random;
 
 public class ZombieMod {
+    private static ItemStack goldenHelmet = new ItemStack(Items.GOLDEN_HELMET);
+    private static ItemStack goldenChestplate = new ItemStack(Items.GOLDEN_CHESTPLATE);
+    private static ItemStack goldenLeggings = new ItemStack(Items.GOLDEN_LEGGINGS);
+    private static ItemStack goldenBoots = new ItemStack(Items.GOLDEN_BOOTS);
+    private static ItemStack goldenSword = new ItemStack(Items.GOLDEN_SWORD);
+
     public static void register() {
 
         ServerEntityEvents.ENTITY_LOAD.register((entity, world) -> {
@@ -17,13 +31,30 @@ public class ZombieMod {
                 return;
             }
             if (!(entity instanceof ZombieEntity zombie) || entity.getType() == EntityType.ZOMBIFIED_PIGLIN
-                    || entity.getType() == EntityType.DROWNED || entity.getType() == EntityType.GIANT) {
+                    || entity.getType() == EntityType.GIANT) {
                 return;
             }
             if (entity.getCommandTags().contains("custom_zombie"))
                 return;
+            if (ModTimeManager.hasPassedDays(10)) {
+                ZombifiedPiglinMod.equipArmor(zombie, List.of(
+                        goldenHelmet,
+                        goldenChestplate,
+                        goldenLeggings,
+                        goldenBoots));
+                zombie.equipStack(EquipmentSlot.MAINHAND, goldenSword);
+                zombie.addCommandTag("custom_zombie");
+            }
+            if (ModTimeManager.hasPassedDays(5) && entity.getType() == EntityType.DROWNED) {
+                EntityAttributeInstance damageAttr = zombie.getAttributeInstance(EntityAttributes.ATTACK_DAMAGE);
+                if (damageAttr != null) {
+                    double baseDamage = damageAttr.getBaseValue();
+                    damageAttr.setBaseValue(baseDamage * 3); // 💥 triplica el daño
+                    zombie.addCommandTag("strong_drowned");
+                }
+            }
             Random random = world.getRandom();
-            if (ModTimeManager.hasPassedDays(20)) {
+            if (ModTimeManager.hasPassedDays(20) && !(entity.getType() == EntityType.DROWNED)) {
                 if (random.nextInt(100) < 10) {
                     GiantEntity giant = EntityType.GIANT.create(
                             world,
